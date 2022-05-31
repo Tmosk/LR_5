547e95d983826fc1a006c38b3f234d11bad9ec50
Tsimophey <t.moskalev.w@gmai.com>
2022-05-31 11:35:43 +0300

    Done

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Tsimophey/CLionProjects/LR_5
+# Install script for directory: C:/Files/toWindows11myProjects/CLionProjects/LR_5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/Tsimophey/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/213.6461.75/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/Tsimophey/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/213.7172.20/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Tsimophey/CLionProjects/LR_5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Files/toWindows11myProjects/CLionProjects/LR_5/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
+#include <malloc.h>
+////////////////////// Stack for char
+typedef struct Stack
+{
+    char data;
+    struct Stack *next;
+} Stack;
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+Stack *push(Stack *stack, char element);
+Stack *get(Stack *stack,char *buf);
+char check(Stack *stack);
+int getSize(Stack *stack);
+
+int main(){
+    setbuf(stdout,0);
+    Stack *lowStack=NULL;
+    Stack *highStack=NULL;
+    printf("Enter stack size:\t");
+    int size;
+    scanf_s("%d",&size);
+    printf("Enter characters:\n");
+    while (getSize(lowStack)<size && getSize(highStack)<size){
+        char buf;
+        scanf_s("%c",&buf);
+        if(buf>=65 && buf<=90){
+            highStack= push(highStack,buf);
+        } else if(buf>=97 && buf<=122){
+            lowStack= push(lowStack,buf);
+        }
+    }
+    printf("High stack:\n");
+    int sizeSt=getSize(highStack);
+    for (int i = 0; i <sizeSt ; ++i) {
+        char buf;
+        highStack=get(highStack,&buf);
+        printf("%c ", buf);
+    }
+    printf("\nLow stack:\n");
+    sizeSt=getSize(lowStack);
+    for (int i = 0; i < sizeSt; ++i) {
+        char buf;
+        lowStack=get(lowStack,&buf);
+        printf("%c ", buf);
+    }
 }
+
+
+Stack *push(Stack *stack, char element) {
+    Stack *new= (Stack*)malloc(sizeof(Stack));
+    new->next=stack;
+    stack=new;
+    new->data=element;
+}
+
+Stack *get(Stack *stack,char *buf) {
+    *buf=stack->data;
+    return stack->next;
+}
+
+int getSize(Stack *stack) {
+    int i=0;
+    while (stack!=NULL){
+      stack=stack->next;
+      ++i;
+    }
+    return i;
+}
+
+char check(Stack *stack) {
+    return stack->data;
+}
+
